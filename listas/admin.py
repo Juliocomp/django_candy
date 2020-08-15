9ae23e1e67466588452c32a15562cae341c6fb4d
juliocomp <jules200102@gmail.com>
2020-08-14 22:13:53 -0500

    Enter

--- listas/admin.py
+++ listas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Tarea_ind
 # Register your models here.
+
+
+admin.site.register(Tarea_ind)
+
+
+
+
+
--- listas/models.py
+++ listas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tarea_ind(models.Model):
+    titulo = models.CharField(max_length=200, default=" ")
+    status = models.BooleanField(default=False)
+    creado = models.DateTimeField(auto_now_add=True, )
+    detalles =models.TextField(max_length=300, default=" g")
+
+    def __str__(self):
+        return self.titulo

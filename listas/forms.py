3bfee6473d5cb4076d499591309a067b8efe46d4
juliocomp <jules200102@gmail.com>
2020-08-14 22:16:57 -0500

    Enter

---
+++ listas/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import ModelForm
+from models import Tarea_ind
+
+
+class Tarea_form(forms.ModelForm):
+    
+    class Meta:
+        model= Tarea_ind
+        fields= '__all__'
+
+
+class Raw_tarea_form(forms.Form):
+    titulo=forms.CharField()
+    status=forms.BooleanField()
+    #creado=forms.DateTimeField()
+    descripcion=forms.CharField()
